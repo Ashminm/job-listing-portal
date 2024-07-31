765ea39b87bdda0af062d1c1d7648f4a5bc3f1ee
Ashmin <ashmin2802@gmail.com>
2024-07-31 15:58:47 +0530

    clean up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-500 text-7xl'>Hello world</h1>
+      <h1 className='bg-info'>bg</h1>
+    </>
+  )
+}
+
+export default App
