09cdea46b31a8b2d7de3315e8ef2998b28e2028a
Ashmin <ashmin2802@gmail.com>
2024-08-20 20:58:55 +0530

    sp

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Landing from './pages/Landing'
 import Home from './pages/Home'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
+    <Header/>
     <Routes>
         <Route path='/' element={<Landing/>}  />
         <Route path='/home' element={<Home/>}  />
-
+        {/* <Route path='' /> */}
         <Route path={'/*'} element={<Home/>} />
       </Routes>
     </>
